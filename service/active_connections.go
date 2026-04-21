d03ae7648b7821289c9b61c91da1c452ef05118a
airtau <airtau@smd.airtau.ru>
2026-04-21 19:43:31 +0300

    отладка

--- service/active_connections.go
+++ service/active_connections.go
@@ -27,8 +27,8 @@ func (as *ActiveConnectionsService) ListActive(page, pageSize uint) (list []*Act
     
     // Запрос к audit_conns с условием action='new' и close_time=0
     tx := DB.Model(&model.AuditConn{}).
-	Where("action = ? AND close_time = ?", "new", 0).
-	Order("id DESC")
+        Where("action = ? AND close_time = ?", "new", 0).
+        Order("id DESC")
     
     tx.Count(&total)
     tx.Scopes(Paginate(page, pageSize)).Find(&auditConns)
@@ -36,51 +36,59 @@ func (as *ActiveConnectionsService) ListActive(page, pageSize uint) (list []*Act
     // Собираем peer_id для запроса к peers
     peerIds := make([]string, 0)
     for _, conn := range auditConns {
-	if conn.PeerID != "" {
-	    peerIds = append(peerIds, conn.PeerID)
-	}
+        if conn.PeerId != "" {
+            peerIds = append(peerIds, conn.PeerId)
+        }
     }
     
     // Загружаем peers
     var peers []*model.Peer
     if len(peerIds) > 0 {
-	DB.Where("id IN ?", peerIds).Find(&peers)
+        DB.Where("id IN ?", peerIds).Find(&peers)
     }
     
     // Создаём карту peer_id -> peer
     peerMap := make(map[string]*model.Peer)
     for _, peer := range peers {
-	peerMap[peer.ID] = peer
+        peerMap[peer.Id] = peer
     }
     
     // Формируем результат
     list = make([]*ActiveConnection, 0)
     for _, conn := range auditConns {
-	activeConn := &ActiveConnection{
-	    ID:        conn.ID,
-	    PeerID:    conn.PeerID,
-	    FromPeer:  conn.FromPeer,
-	    FromName:  conn.FromName,
-	    FromIP:    conn.IP,
-	    UUID:      conn.UUID,
-	    CreatedAt: conn.CreatedAt.Format("2006-01-02 15:04:05"),
-	    Action:    conn.Action,
-	    CloseTime: conn.CloseTime,
-	}
-	
-	// Добавляем данные из peers
-	if peer, ok := peerMap[conn.PeerID]; ok {
-	    activeConn.Hostname = peer.Hostname
-	    activeConn.TargetIP = peer.LastOnlineIP
-	    if activeConn.TargetIP == "" {
-		activeConn.TargetIP = peer.IP
-	    }
-	} else {
-	    activeConn.Hostname = "-"
-	    activeConn.TargetIP = "-"
-	}
-	
-	list = append(list, activeConn)
+        // Преобразуем AutoTime в строку
+        var createdAtStr string
+        if t, ok := interface{}(conn.CreatedAt).(interface{ Format(string) string }); ok {
+            createdAtStr = t.Format("2006-01-02 15:04:05")
+        } else {
+            createdAtStr = conn.CreatedAt.String()
+        }
+        
+        activeConn := &ActiveConnection{
+            ID:        conn.Id,
+            PeerID:    conn.PeerId,
+            FromPeer:  conn.FromPeer,
+            FromName:  conn.FromName,
+            FromIP:    conn.Ip,
+            UUID:      conn.Uuid,
+            CreatedAt: createdAtStr,
+            Action:    conn.Action,
+            CloseTime: conn.CloseTime,
+        }
+        
+        // Добавляем данные из peers
+        if peer, ok := peerMap[conn.PeerId]; ok {
+            activeConn.Hostname = peer.Hostname
+            activeConn.TargetIP = peer.LastOnlineIp
+            if activeConn.TargetIP == "" {
+                activeConn.TargetIP = peer.Ip
+            }
+        } else {
+            activeConn.Hostname = "-"
+            activeConn.TargetIP = "-"
+        }
+        
+        list = append(list, activeConn)
     }
     
     return list, total, nil
